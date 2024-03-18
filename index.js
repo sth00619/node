5dfd57cb04651072ce75dc463efc2577f4561b4e
sth00619 <alpngmu99@gmail.com>
2024-03-18 16:11:38 +0900

    save

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-var figlet = require("figlet");
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = 3000;
 
-figlet("Hello World!!", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
+app.use(cors());
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/user/:id", function (req, res) {
+  // const q = req.params
+  // console.log(q.id)
+
+  const q = req.query;
+  console.log(q.q);
+  console.log(q.name);
+  console.log(q.age);
+
+  res.json({ userid: q.name });
+});
+
+app.get("/sound/:name", function (req, res) {
+  const { name } = req.params;
+  if (name == "dog") {
+    res.json({ sound: "멍멍" });
+  } else if (name == "cat") {
+    res.json({ sound: "야옹" });
+  } else if (name == "pig") {
+    res.json({ sound: "꿀꿀" });
+  } else {
+    res.json({ sound: "알 수 없음" });
   }
-  console.log(data);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
